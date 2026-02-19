bc41b7ae9d7e16224d98bfacd230ff1ccd86b790
herman <herman.anez.dev@gmail.com>
2026-02-19 14:00:50 +0000

    add debounce to search

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default async function Page(props: {
