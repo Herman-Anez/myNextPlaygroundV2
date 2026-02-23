54761a8e17f30ac3fc627803573bb5c6ae785c9c
herman <herman.anez.dev@gmail.com>
2026-02-23 14:33:00 +0000

    add State type

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,6 +6,15 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!);
 const FormSchema = z.object({
     id: z.string(),
