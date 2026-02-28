b6f4940d0e078c5be1cd80960a0ec03384ab23c9
herman <herman.anez.dev@gmail.com>
2026-02-28 17:14:43 +0100

    se implemento la autenticacion

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,10 +1,34 @@
 'use server';
 
 import { z } from 'zod';
-
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        console.log("------------------------------------")
+        console.log(error.type)
+         console.log("------------------------------------")
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 
 export type State = {
     errors?: {
