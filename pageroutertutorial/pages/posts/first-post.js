44db873b38499922398cd21b2e0000ae6ab120e2
herman <herman.anez.dev@gmail.com>
2026-03-01 19:38:15 +0000

    add external js

--- pageroutertutorial/pages/posts/first-post.js
+++ pageroutertutorial/pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/layout';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
@@ -8,7 +9,14 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-            <h1>First Post</h1>
+             {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }
+            />            <h1>First Post</h1>
             <h2>
                 <Link href="/">← Back to home</Link>
             </h2>
